68e09a74ba0993bdd1bdcb5120b8ba78b5a1aaee
Nicolai Cornelis <nickdnk@hotmail.com>
2024-10-24 22:00:54 +0200

    Add JSON schema Add health_check_timeout option

--- config.go
+++ config.go
@@ -6,6 +6,8 @@ import "net/http"
 type Config struct {
 	// Address of the http server
 	Address string
+	// Time to wait for a health check response.
+	HealthCheckTimeout int `mapstructure:"health_check_timeout"`
 	// Status code returned in case of fail, 503 by default
 	UnavailableStatusCode int `mapstructure:"unavailable_status_code"`
 }
@@ -14,5 +16,8 @@ type Config struct {
 func (c *Config) InitDefaults() {
 	if c.UnavailableStatusCode == 0 {
 		c.UnavailableStatusCode = http.StatusServiceUnavailable
+	}
+	if c.HealthCheckTimeout <= 0 {
+		c.HealthCheckTimeout = 60
 	}
 }
